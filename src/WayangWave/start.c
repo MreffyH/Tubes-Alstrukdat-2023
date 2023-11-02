ca178bb407e831bce662599f3fdcf50d6e93159e
MreffyH <reffyhaykal@gmail.com>
2023-11-02 18:10:51 +0700

    update Start

--- src/WayangWave/start.c
+++ src/WayangWave/start.c
@@ -4,15 +4,28 @@
 
 boolean stateWayangWave;
 
-void start(List Penyanyi, Map Album, Set Lagu) {
-    if (!IsEmpty(Penyanyi)) {
-        printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
-        stateWayangWave = true;
-    } else {
-        stateWayangWave = false;
+void start(List *Penyanyi, Map *Album, Set *Lagu) {
+    STARTWORD();
+    while(!isEndWord()) {
+        int singer;
+        singer = strToInteger(currentWord);
+        ADVWORD();
+        int i;
+        while (i <= singer) 
+        {
+            CreateEmpty(Penyanyi);
+            i++;
+        }
     }
+    
 }
 /*  Membaca file konfigurasi default yang berisi list lagu yang dapat dimainkan.
     I.S. StateSPOTIFY bernilai sembarang.
     F.S. File konfigurasi terbaca dan list lagu bertambah. StateSPOTIFY true.
 */
+// if (!IsEmpty(*Penyanyi)) {
+//         printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
+//         stateWayangWave = true;
+//     } else {
+//         stateWayangWave = false;
+//     }
--- src/WayangWave/start.h
+++ src/WayangWave/start.h
@@ -6,7 +6,7 @@
 #include "../adt/map.h"
 #include "../adt/listlinier.h"
 
-void start(List Penyanyi, Map Album, Set Lagu);
+void start(List *Penyanyi, Map *Album, Set *Lagu);
 /*  Membaca file konfigurasi default yang berisi list lagu yang dapat dimainkan.
     I.S. StateWayangWave bernilai sembarang.
     F.S. File konfigurasi terbaca dan list lagu bertambah. StateWayangWave true.
