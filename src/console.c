99cf66b062fc93f5d96e6f734fabaef9ae2b8978
TjapDjae77 <r.f.rayhan@gmail.com>
2023-11-02 15:12:59 +0700

    Update console.c
    Buat template semua commands

---
+++ src/console.c
@@ -0,0 +1,89 @@
+#include "console.h"
+
+
+void Start(){
+
+}
+
+void Load(){
+
+}
+
+void ListDefault(){
+    
+}
+
+void ListPlaylist(){
+    
+}
+
+void PlaySong(){
+    
+}
+
+void PlayPlaylist(){
+    
+}
+
+void QueueSong(){
+    
+}
+
+void QueuePlaylist(){
+    
+}
+
+void QueueSwap(int x, int y){
+    
+}
+
+void QueueRemove(int x){
+    
+}
+
+void QueueClear(){
+    
+}
+
+void SongNext(){
+    
+}
+
+void SongPrevious(){
+    
+}
+
+void PlaylistCreate(){
+    
+}
+
+void PlaylistAdd(char *input){
+    
+}
+
+void PlaylistSwap(int id, int x, int y){
+    
+}
+
+void PlaylistRemove(int id, int n){
+    
+}
+
+void PlaylistDelete(){
+    
+}
+
+void Status(){
+    
+}
+
+void Save(){
+    
+}
+void Quit(){
+    
+}
+
+void Help(){
+    
+}
