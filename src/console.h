839cdcc800bc947d853b41eb8029bb66e090e20a
TjapDjae77 <r.f.rayhan@gmail.com>
2023-11-02 14:41:54 +0700

    Update console.h
    Isi console (argumen belum lengkap)

---
+++ src/console.h
@@ -0,0 +1,53 @@
+#include "../ADT/list.h"
+#include "../ADT/listlinier.h"
+#include "../ADT/map.h"
+#include "../ADT/mesinkarakter.h"
+#include "../ADT/mesinkata.h"
+#include "../ADT/queue.h"
+#include "../ADT/set.h"
+#include "../ADT/stack.h"
+#include "../WayangWave/start.h"
+
+void Start();
+
+void Load();
+
+void ListDefault();
+
+void ListPlaylist();
+
+void PlaySong();
+
+void PlayPlaylist();
+
+void QueueSong();
+
+void QueuePlaylist();
+
+void QueueSwap(int x, int y);
+
+void QueueRemove(int x);
+
+void QueueClear();
+
+void SongNext();
+
+void SongPrevious();
+
+void PlaylistCreate();
+
+void PlaylistAdd(char *input);
+
+void PlaylistSwap(int id, int x, int y);
+
+void PlaylistRemove(int id, int n);
+
+void PlaylistDelete();
+
+void Status();
+
+void Save();
+
+void Quit();
+
+void Help();
