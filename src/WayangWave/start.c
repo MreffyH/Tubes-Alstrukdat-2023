9a3f63b4c5217322a05710bebb62e8101c6595ff
TjapDjae77 <r.f.rayhan@gmail.com>
2023-11-02 13:28:56 +0700

    Update start.c
    Nambahin fopen

--- src/WayangWave/start.c
+++ src/WayangWave/start.c
@@ -1,13 +1,16 @@
 #include "start.h"
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 boolean stateWayangWave;
+FILE *fc;
 
 void start(List *PlaylistLagu, List Penyanyi, Set Lagu) {
 
     if (!IsEmpty(*PlaylistLagu)) {
         printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
         stateWayangWave = true;
+        *fc = fopen("config.txt", "w");
     } else {
         stateWayangWave = false;
     }
