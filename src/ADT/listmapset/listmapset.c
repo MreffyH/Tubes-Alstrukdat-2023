ec4f85681a36374bd9e49f90aeea4f990ef1568f
TjapDjae77 <r.f.rayhan@gmail.com>
2023-11-18 22:29:26 +0700

    Buat listmapset

--- src/ADT/listmapset/listmapset.c
+++ src/ADT/listmapset/listmapset.c
@@ -1,2 +1,64 @@
-#include"listmapset.h"
-#include<stdio.h>
+#include "listmapset.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void CreateListPenyanyi(ListPenyanyi *penyanyi){
+    Jumlahpenyanyi(*penyanyi) = 0;
+}
+
+void AddPenyanyi(ListPenyanyi *penyanyi, Sentence nama){
+    (*penyanyi).penyanyi_ke[Jumlahpenyanyi(*penyanyi)].namaPenyanyi = nama;
+    (*penyanyi).penyanyi_ke[Jumlahpenyanyi(*penyanyi)].countalbum = 0;
+    Jumlahpenyanyi(*penyanyi)++;
+}
+
+int SearchPenyanyi_ke(ListPenyanyi penyanyi, Sentence namaPenyanyi){
+    int i = 0;
+    boolean found = false;
+    while ((i < Jumlahpenyanyi(penyanyi)) && (!found)){
+        if(IsKalimatEqual((penyanyi).penyanyi_ke[i].namaPenyanyi, namaPenyanyi)){
+            found = true;
+        }
+        else{
+            i++;
+        }
+    } /* i == Jumlahpenyanyi(*penyanyi) or found */
+    if (found){
+        return i;
+    }
+    else{
+        return NOTFOUND;
+    }
+}
+void AddAlbum(ListPenyanyi *penyanyi, Sentence namaPenyanyi, Sentence namaAlbum){
+    int urutan = SearchPenyanyi_ke(*penyanyi, namaPenyanyi);
+    if(urutan != NOTFOUND){
+        (*penyanyi).penyanyi_ke[urutan].listalbum[Jumlahalbum((*penyanyi).penyanyi_ke[urutan])].namaAlbum = namaAlbum; 
+        Jumlahalbum((*penyanyi).penyanyi_ke[urutan])++;
+    }
+}
+
+void AddLagu(ListPenyanyi *penyanyi, Sentence namaPenyanyi, Sentence namaAlbum, Sentence namaLagu){
+    /* cari penyanyi */
+    int urutan = SearchPenyanyi_ke(*penyanyi, namaPenyanyi);
+    if(urutan != NOTFOUND){
+        /* cari album */
+        int i = 0;
+        boolean found = false;
+        while ((i < (*penyanyi).penyanyi_ke[urutan].countalbum) && (!found)){
+            if (IsKalimatEqual((*penyanyi).penyanyi_ke[urutan].listalbum[i].namaAlbum, namaAlbum)){
+                found = true;
+            }
+            else{
+                i++;
+            }
+        } /* i == jumlahalbum or found */
+        if(found){
+            /* cari apakah ada lagu yang sama */
+            if(!IsMemberSet((*penyanyi).penyanyi_ke[urutan].listalbum[i].setlagu, namaLagu)){
+                InsertSet(&((*penyanyi).penyanyi_ke[urutan].listalbum[i].setlagu), namaLagu);
+            }
+        }
+    }
+}
--- src/ADT/listmapset/listmapset.h
+++ src/ADT/listmapset/listmapset.h
@@ -1,4 +1,37 @@
 #include "../map & set/map.h"
 #include "../map & set/set.h"
 #include "../list/list.h"
-#include<stdio.h>
+#include "../mesin/mesinkalimat.h"
+
+#define MaxElAlbum 40
+#define MaxElSinger 50
+#define NOTFOUND -1
+
+typedef struct{
+	Sentence namaAlbum;
+	Set setlagu;
+} MapAlbum;
+
+typedef struct{
+	Sentence namaPenyanyi;
+	MapAlbum listalbum[MaxElAlbum];
+	int countalbum;
+} MapPenyanyi;
+
+typedef struct{
+	MapPenyanyi penyanyi_ke[MaxElSinger];
+	int countpenyanyi;
+} ListPenyanyi;
+
+#define Jumlahpenyanyi(L) (L).countpenyanyi
+#define Jumlahalbum(L) (L).countalbum
+
+void CreateListPenyanyi(ListPenyanyi *penyanyi);
+
+void AddPenyanyi(ListPenyanyi *penyanyi, Sentence nama);
+
+int SearchPenyanyi_ke(ListPenyanyi penyanyi, Sentence namaPenyanyi);
+
+void AddAlbum(ListPenyanyi *penyanyi, Sentence namaPenyanyi, Sentence namaAlbum);
+
+void AddLagu(ListPenyanyi *penyanyi, Sentence namaPenyanyi, Sentence namaAlbum, Sentence namaLagu);
