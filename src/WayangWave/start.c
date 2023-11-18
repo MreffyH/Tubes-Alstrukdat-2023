dfa664069d615cc02ceceb8f742bff8a389bcc35
MreffyH <reffyhaykal@gmail.com>
2023-11-18 22:43:42 +0700

    Update Start

--- src/WayangWave/start.c
+++ src/WayangWave/start.c
@@ -4,15 +4,40 @@
 
 boolean stateWayangWave;
 
-void start(List *Penyanyi, Map *Album, Set *Lagu) {
-    bacaConfig("config.txt", Penyanyi, Album, Lagu);
-    if (!IsEmpty(*Penyanyi)) {
-        printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
-        stateWayangWave = true;
-    } else {
-        stateWayangWave = false;
+void start(char *input, List *Penyanyi, Map *Album, Set *Lagu) {
+    STARTFILE(input);
+    int jumlahPenyanyi = 0;
+    int jumlahAlbum = 0;
+    int jumlahLagu = 0;
+
+    /* Angka Jumlah Penyanyi*/
+    jumlahPenyanyi = strToInteger(currentWord);
+
+    /* Berdasarkan Jumlah Penyanyi...*/
+    for (int inSinger = 0; inSinger < jumlahPenyanyi; inSinger++) { // di dalam penyanyi
+        ADVWORD(); 
+        /* Angka Jumlah Album */
+        jumlahAlbum = strToInteger(currentWord);
+        
+        AcquireSentence();
+        Sentence PenyanyiNow = currentKalimat;
+        AddPenyanyi(&Penyanyi, currentKalimat); // Input penyanyi ke list
+        /* Berdasarkan Jumlah Album... */
+        for (int inAlbum = 0; inAlbum < jumlahAlbum; inAlbum++) { // di dalam album
+            ADVWORD(); // baris ketiga angka
+            /* Angka Jumlah Lagu */
+            jumlahLagu = strToInteger(currentWord);
+            AcquireSentence();
+            Sentence AlbumNow = currentKalimat;
+            AddAlbum(&Penyanyi, PenyanyiNow, currentKalimat); // input album ke map
+            /* Berdasarkan Jumlah Lagu...*/
+            for (int i = 0; i < jumlahLagu; i++) { // Input lagu ke set
+                AcquireSentence();
+                AddLagu(&Penyanyi, PenyanyiNow, AlbumNow, currentKalimat);
+            }
+        }
     }
-}
+}  
 /*  Membaca file konfigurasi default yang berisi list lagu yang dapat dimainkan.
     I.S. StateSPOTIFY bernilai sembarang.
     F.S. File konfigurasi terbaca dan list lagu bertambah. StateSPOTIFY true.
--- src/WayangWave/start.h
+++ src/WayangWave/start.h
@@ -1,13 +1,10 @@
 #ifndef START_WayangWave_H
 #define START_WayangWave_H
 
-#include "../adt/mesinkata.h"
-#include "../adt/set.h"
-#include "../adt/map.h"
-#include "../adt/listlinier.h"
-#include "bacaConfig.h"
+#include "../ADT/listmapset/listmapset.h"
+#include "../ADT/mesin/mesinkalimat.h"
 
-void start(List *Penyanyi, Map *Album, Set *Lagu);
+void start(char *input, List *Penyanyi, Map *Album, Set *Lagu);
 /*  Membaca file konfigurasi default yang berisi list lagu yang dapat dimainkan.
     I.S. StateWayangWave bernilai sembarang.
     F.S. File konfigurasi terbaca dan list lagu bertambah. StateWayangWave true.
