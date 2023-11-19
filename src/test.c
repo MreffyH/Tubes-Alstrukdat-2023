b4edd4206a16b7f81f20bee83328375afd473964
TjapDjae77 <r.f.rayhan@gmail.com>
2023-11-19 15:34:45 +0700

    ubah-ubah

--- src/test.c
+++ src/test.c
@@ -1,20 +1,21 @@
-#include "WayangWave/start/start.h"
-#include "ADT/listmapset/listmapset.h"
-#include "ADT/mapset/set.h"
-#include "ADT/mesin/mesinkalimat.h"
-#include "ADT/mesin/mesinkata.h"
-#include "ADT/mesin/mesinkarakter.h"
+// #include "WayangWave/start/start.h"
+// #include "ADT/listmapset/listmapset.h"
+// #include "ADT/mapset/set.h"
+// #include "ADT/mesin/mesinkalimat.h"
+// #include "ADT/mesin/mesinkata.h"
+// #include "ADT/mesin/mesinkarakter.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main() {
     // Membuat ListPenyanyi
-    printf("CEK\n");
-    ListPenyanyi Penyanyi;
-    CreateListPenyanyi(&Penyanyi);
+    printf("CEK1\n");
+    // ListPenyanyi Penyanyi;
+    // CreateListPenyanyi(&Penyanyi);
+    printf("CEK2\n");
 
     // Memanggil fungsi start dengan file input "contoh.txt"
-    start("config.txt", &Penyanyi);
+    // start("config.txt", &Penyanyi);
 
     // Menampilkan hasil dari ListPenyanyi (ini hanya contoh, sesuaikan dengan struktur data Anda)
     // Misalnya, Anda memiliki fungsi PrintListPenyanyi untuk menampilkan isi ListPenyanyi.
@@ -22,6 +23,6 @@ int main() {
 
     // Hapus ListPenyanyi setelah selesai digunakan
     // DeleteList(&Penyanyi);
-    printf("CEK\n");
+    printf("CEK3\n");
     return 0;
 }
