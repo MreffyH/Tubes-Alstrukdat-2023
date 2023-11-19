e037989525cc920602762e62a7737c9da25e15d2
rivaldimhr <rivaldimhr@gmail.com>
2023-11-19 13:51:14 +0700

    coba

--- src/WayangWave/listadt.c
+++ src/WayangWave/listadt.c
@@ -1,39 +1,41 @@
 #include "list.h"
 #include <stdio.h>
-// list default
+#include <stdlib.h>
+#include "list.c"
 #include "start.h"
+#include "arraydin.c"
 
-void listdefault(List *s)
+void listDefault(List *s, char name_penyanyi, char name_album, char name_lagu)
 {
+    Fullarray(s);
     printf(">> LIST DEFAULT;\n");
     int i;
-    printf("Daftar Penyanyi:\n");
-    for (i = 0; i < Length(*s); i++)
-    {
-        printf("%d. %s\n", i + 1, currentWord);
-    }
+    char *b = arrayfirst();
+    MakeList(s);
+    listdefault(s);
 
     char choice;
     if (choice == 'Y' || choice == 'y')
     {
-        char name;
-        printf("Pilih penyanyi untuk melihat album mereka: \n%s", name);
-        printf("Daftar Album oleh %s:\n", currentWord);
-        for (i = 0; i < length(); i++)
+        if (Search(s, name_penyanyi))
         {
-            printf("%d. %s\n", i + 1, map_H);
-        }
-        printf("Masukkan Nama Album yang dipilih : \n%s", currentWord);
-
-        char choice2;
-        if (choice2 == 'Y' || choice2 == 'y')
-        {
-            printf("Ingin melihat lagu yang ada?(Y/N): Y;");
-            printf("Pilih album untuk melihat lagu yang ada di album :\n %s;", currentWord);
-            printf("Daftar Lagu di %s:\n", currentWord);
-            for (i = 0; i < album.count; i++)
+            printf("Pilih penyanyi untuk melihat album mereka: \n%s", name_penyanyi);
+            printf("Daftar Album oleh %s:\n", name_penyanyi);
+            ListAlbumPenyanyi(s, name_penyanyi);
+            if (Search(*s, name_album))
             {
-                printf("%d. %s\n", i + 1, set_H);
+                printf("Masukkan Nama Album yang dipilih : \n%s\n", name_album);
+                char choice2;
+                if (choice2 == 'Y' || choice2 == 'y')
+                {
+                    printf("Ingin melihat lagu yang ada?(Y/N): %s;", choice2);
+                    printf("Pilih album untuk melihat lagu yang ada di album :\n %s;\n", name_album);
+                    lagudanalbum(s, name_album, name_penyanyi);
+                }
+            }
+            else
+            {
+                printf("Tidak ada nama album yang sesuai");
             }
         }
     }
@@ -43,7 +45,7 @@ void playlist(List *s)
 {
     printf(">> LIST PLAYLIST;\n");
     int i;
-    if (playlist(*s) == NULL)
+    if (s == NULL)
     {
         printf("Daftar playlist yang kamu miliki:\n");
         printf("Kamu tidak memiliki playlist.\n");
@@ -51,10 +53,10 @@ void playlist(List *s)
     }
     else
     {
-        for (i = 0; i < length(); i++)
+        printf("Daftar playlist yang kamu miliki:\n");
+        for (i = 0; i < Playlist.A; i++)
         {
-            printf("Kamu tidak memiliki playlist.\n");
-            printf("%d. %s\n", i + 1, playlist(s));
+            printf("%d. %s\n", i + 1, Playlist.A[i]);
         }
     }
 }
