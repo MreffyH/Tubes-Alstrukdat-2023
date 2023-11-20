e96632f10b1f640438cd39abae907c521f54e209
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2023-11-20 22:12:19 +0700

    nyoba status

--- src/WayangWave/status.c
+++ src/WayangWave/status.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 // Definisi struktur lagu
 typedef struct {
-    char title[100];
-    char album[100];
-    char artist[100];
+    char *title;
+    char *album;
+    char *artist;
 } Song;
 
 // Definisi struktur node untuk queue
@@ -23,7 +22,7 @@ typedef struct {
 
 // Definisi struktur untuk playlist
 typedef struct {
-    char name[100];
+    char *name;
     Queue songs; // Misalnya, playlist menggunakan queue juga untuk menyimpan lagu
 } Playlist;
 
@@ -34,12 +33,13 @@ int isPlaylistActive = 0;
 
 // Fungsi untuk menampilkan status
 void printStatus(Queue* queue) {
-    if (isPlaylistActive) {
-        printf("\nCurrent Playlist: %s\n", currentPlaylist.name);
+    if (isPlaylistActive = 1 && currentPlaylist.name != NULL) {
+        printf("Current Playlist: %s\n", currentPlaylist.name);
+        printf("\n");
     }
 
     printf("Now Playing:\n");
-    if (strlen(nowPlaying.title) == 0) {
+    if (nowPlaying.title == NULL) {
         printf("No songs have been played yet. Please search for a song to begin playback.\n");
     } else {
         printf("%s - %s - %s\n", nowPlaying.artist, nowPlaying.title, nowPlaying.album);
@@ -57,8 +57,33 @@ void printStatus(Queue* queue) {
             count++;
         }
     }
+}
 
+// Fungsi untuk membuat lagu baru dengan string baru
+Song createSong(char *title, char *artist, char *album) {
+    Song newSong;
+    newSong.title = title;
+    newSong.artist = artist;
+    newSong.album = album;
+    return newSong;
+}
 
+// Fungsi untuk menambahkan lagu ke dalam queue
+void enqueue(Queue *queue, Song newSong) {
+    QueueNode *newNode = (QueueNode *)malloc(sizeof(QueueNode));
+    if (newNode == NULL) {
+        printf("Memory allocation failed!\n");
+        return;
+    }
+    newNode->song = newSong;
+    newNode->next = NULL;
+
+    if (queue->rear == NULL) {
+        queue->front = queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
 }
 
 // Fungsi utama
@@ -66,16 +91,19 @@ int main() {
     Queue queue = {NULL, NULL}; // Inisialisasi queue kosong
 
     // Contoh penggunaan
-    strcpy(nowPlaying.title, "Up&Up");
-    strcpy(nowPlaying.artist, "Coldplay");
-    strcpy(nowPlaying.album, "A Head Full of Dreams");
+    nowPlaying = createSong("Up&Up", "Coldplay", "A Head Full of Dreams");
 
     // Contoh menambahkan lagu ke queue
-    // ... (kode untuk menambahkan lagu ke queue)
+    Song newSong1 = createSong("Jiwaru Days", "JKT48", "Mahagita Vol.2");
+    enqueue(&queue, newSong1);
+
+    // Song newSong2 = createSong("Jurus Rahasia Teleport", "JKT48", "Pajama Drive");
+    // enqueue(&queue, newSong2);
 
     // Contoh menetapkan playlist aktif
-    isPlaylistActive = 1;
-    strcpy(currentPlaylist.name, "Copium");
+    isPlaylistActive = 2;
+    // currentPlaylist.name = "Copium";
+    // currentPlaylist.name = "Pajama Drive";
 
     // Menampilkan status
     printStatus(&queue);
