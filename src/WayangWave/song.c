8e50189465083bebdee9398669d0b822202bce59
YusrilMahendraa <yusrilfazri14@gmail.com>
2023-11-21 22:27:42 +0700

    update song.c

--- src/WayangWave/song.c
+++ src/WayangWave/song.c
@@ -2,32 +2,42 @@
 #include "queue.h"
 #include <stdio.h>
 
-void songNext(Queue *q, Stack *s, ElType x)
+void songNext(Queue *q, Stack *s, currentsong x)
 {
     if (isEmpty(*q))
     {
         printf("Queue kosong, memutar kembali lagu");
-        printf(" \" % c \" oleh \"%c\" ");
+        printf(/*currentsong*/);
+        printf("oleh");
+        printf(/*currentsong*/);
     }
     else
     {
         dequeue(q, &x);
         Push(s, x);
-        printf("");
+        printf("Memutar lagu selanjutnya");
+        printf(/*currentsong*/);
+        printf("oleh");
+        printf(/*currentsong*/)
     }
 }
 
-void songPrevious(Queue *q, Stack *s, ElType x)
+void songPrevious(Queue *q, Stack *s, currentsong x)
 {
     if (isEmpty(*q))
     {
-        printf("memutar lagu sebelumnya");
-        printf(" \" % c \" oleh \"%c\" ");
+        printf("Riwayat lagu kosong, memutar kembali lagu");
+        printf(/*current song*/);
+        printf("oleh");
+        printf(/*currentsong*/);
     }
     else
     {
         Pop(s, &x);
         Push(s, x);
-        printf("");
+        printf("Memutar lagu sebelumnya");
+        printf(/*currentsong*/);
+        printf("oleh");
+        printf(/*currentsong*/);
     }
 }
