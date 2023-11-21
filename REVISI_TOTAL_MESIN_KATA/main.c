4354616376630a48b5fae12a74f4a02675d78d6d
MreffyH <reffyhaykal@gmail.com>
2023-11-21 09:38:46 +0700

    UPDATE MAIN

--- REVISI_TOTAL_MESIN_KATA/main.c
+++ REVISI_TOTAL_MESIN_KATA/main.c
@@ -9,13 +9,26 @@
 
 int main(){
     boolean mulaiWayangWave = true;
-    while (mulaiWayangWave)
-    {
+    boolean dalamsesi = false;
+
+
+
+    printf("\n-------SELAMAT DATANG DI PROGRAM WAYANGWAVE-------\n");
+    while (mulaiWayangWave && dalamsesi == false)
+    {   
+        printf("\n");
+        printf(">> ");
         STARTINPUT();
+        printf("\n");
         if (INPUTEQUAL(currentInput, "START")) {
-            printf("masuk ke start\n");
+            printf("MASUK KE START\n");
             char *file = "user1.txt";
             STARTWAYANGWAVE(file);
+            printf("File konfigurasi aplikasi berhasil dibaca. WayangWave berhasil dijalankan.\n");
+        } else if (INPUTEQUAL(currentInput, "LOAD")) {
+            printf("MASUK KE LOAD\n");
+        } else { /* BUAT YANG LAINNYA DAN INVALID COMMAND */
+            printf("\n");
         }
     }
 }
