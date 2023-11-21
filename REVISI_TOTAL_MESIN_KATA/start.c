b7d05df220cc6d84802610b388fe0a5678f512c9
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2023-11-22 01:16:48 +0700

    start test

--- REVISI_TOTAL_MESIN_KATA/start.c
+++ REVISI_TOTAL_MESIN_KATA/start.c
@@ -9,40 +9,40 @@ void STARTWAYANGWAVE(ListPenyanyi *PL, char *filestart) {
     ADV();
     ADVWORD();
     int jumlahpenyanyi = strToInteger(currentWord);
-    printf("%d\n", jumlahpenyanyi);
+    // printf("%d\n", jumlahpenyanyi);
     ADV();
     IgnoreEnter();
     for (int i = 0; i < jumlahpenyanyi; i++){
         ADVWORD();
         int jumlahalbum = strToInteger(currentWord);
-        printf("%d ", jumlahalbum);
+        // printf("%d ", jumlahalbum); 
         ADVSENTENCE();
         IgnoreEnter();
-        printWord(kalimat);
-        printf("\n");
+        // printWord(kalimat);
+        // printf("\n");
         Word PenyanyiNow = DuplicateKata(kalimat);
         AddPenyanyi(PL, kalimat);
         for (int j = 0; j < jumlahalbum; j++){
             ADVWORD();
             int jumlahlagu = strToInteger(currentWord);
-            printf("%d ", jumlahlagu);
+            // printf("%d ", jumlahlagu);
             ADVSENTENCE();
             IgnoreEnter();
-            printWord(kalimat);
-            printf("\n");
+            // printWord(kalimat);
+            // printf("\n");
             Word AlbumNow = DuplicateKata(kalimat);
             AddAlbum(PL, PenyanyiNow, kalimat);
             for(int k = 0; k < jumlahlagu; k++){
                 if((i == jumlahpenyanyi-1) && (j == jumlahalbum-1) && (k == jumlahlagu-1)){
                     ADVSENTENCE();
-                    printWord(kalimat);
+                    // printWord(kalimat);
                     AddLagu(PL, PenyanyiNow, AlbumNow, kalimat);
                 }
                 else{
                     ADVSENTENCE();
                     IgnoreEnter();
-                    printWord(kalimat);
-                    printf("\n");
+                    // printWord(kalimat);
+                    // printf("\n");
                     AddLagu(PL, PenyanyiNow, AlbumNow, kalimat);
                 }
             }
