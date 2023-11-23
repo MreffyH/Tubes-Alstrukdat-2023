aed9a31e5716d362ba7ce5ca4e7a40dc32f92498
rivaldimhr <rivaldimhr@gmail.com>
2023-11-24 03:28:54 +0700

    CEK

--- REVISI_TOTAL_MESIN_KATA/SPESIFIKASI_WAYANGWAVE/FUNGSI_QUEUE/driverQUEUE.c
+++ REVISI_TOTAL_MESIN_KATA/SPESIFIKASI_WAYANGWAVE/FUNGSI_QUEUE/driverQUEUE.c
@@ -1,7 +1,7 @@
 #include "QUEUE_WW.h"
 #include "../../ADT_WAYANGWAVE/STACK/stack.h"
 #include "../LOAD/load.h"
-// gcc -o driverQUEUE driverQUEUE.c ../LOAD/load.c ../START/start.c ../../mesinkarakter.c ../../mesinkata.c ../../listmapset.c ../../../src/ADT/mapset/set.c ../../ADT_WAYANGWAVE/QUEUE/queue.c ../../ADT_WAYANGWAVE/STACK/stack.c ../../../src/ADT/list/listlinier.c ../../../RIvaldi/arraydin.c playlist.c
+// gcc -o driverQUEUE driverQUEUE.c ../LOAD/load.c ../START/start.c ../../mesinkarakter.c ../../mesinkata.c ../../listmapset.c ../../../src/ADT/mapset/set.c ../../ADT_WAYANGWAVE/QUEUE/queue.c ../../ADT_WAYANGWAVE/STACK/stack.c ../../../src/ADT/list/listlinier.c ../../../RIvaldi/arraydin.c QUEUE_WW.c
 
 int main()
 {
@@ -32,4 +32,6 @@ int main()
     // PrintArrayDin(PLY);
     char *file = "../../user1.txt";
     LOADWAYANGWAVE(&PL, file, &QS, &HS, &PLY, &currentSong);
+    GetLagu(PL, currentSong.namaPenyanyiQ, currentSong.namaAlbumQ, 3);
+    QUEUESONG(PL, &QS);
 }
