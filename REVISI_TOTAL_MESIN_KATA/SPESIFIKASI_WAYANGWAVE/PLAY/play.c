194bd5aded7fe2b13fde83223c6d3c1302934d2b
rivaldimhr <rivaldimhr@gmail.com>
2023-11-23 14:53:52 +0700

    done

--- REVISI_TOTAL_MESIN_KATA/SPESIFIKASI_WAYANGWAVE/PLAY/play.c
+++ REVISI_TOTAL_MESIN_KATA/SPESIFIKASI_WAYANGWAVE/PLAY/play.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 // #include "main.c"
 #include "play.h"
+#include "arraydin.h"
 
 void PLAYSONG(ListPenyanyi penyanyi)
 {
@@ -71,37 +72,23 @@ void PLAYSONG(ListPenyanyi penyanyi)
     }
 }
 
-// void playPlaylist(List *L, const char *penyanyi, const char *album, const char *lagu, int idLagu)
-// {
-//     Fullarray(L);
-//     playsong(L, penyanyi, album, lagu, idLagu);
-
-//     int i = 0;
-//     // Iterasi melalui list dan cari lagu berdasarkan penyanyi, album, dan ID lagu
-//     while (i < (*L).size && (customStrCmp((*L).data[i].nama1, penyanyi) != 0 || customStrCmp((*L).data[i].nama2, album) != 0 || atoi((*L).data[i].id) != idLagu))
-//     {
-//         i++;
-//     }
-
-//     // Cek apakah lagu ditemukan
-//     if (i < (*L).size)
-//     {
-//         // Mengisi ulang queue dengan lagu dari playlist
-//         // (Misalnya, menambahkan semua lagu dalam playlist ke dalam queue dengan urutan yang di-reverse)
-//         int playlistSize = getPlaylistSize(&myPlaylist); // Ganti dengan fungsi yang sesuai di program Anda
-//         for (int j = 0; j < playlistSize; j++)
-//         {
-//             // enqueue(&myQueue, getPlaylistSong(&myPlaylist, j)); // Ganti dengan fungsi yang sesuai di program Anda
-//         }
-
-//         // Output untuk menunjukkan bahwa playlist dimainkan
-//         printf("Memutar playlist \"%s\".\n", lagu);
-//     }
-//     else
-//     {
-//         printf("Playlist dengan ID %d tidak ditemukan.\n", idlagu);
-//     }
-
-//     // Pastikan untuk membebaskan memori jika diperlukan
-//     // freePlaylist(&myPlaylist); // Ganti dengan fungsi yang sesuai di program Anda
-// }
+void playPlaylist(ArrayDin array)
+{
+    if (!IsEmptyArrayDin(array))
+    {
+        for (int i = 0; i < LengthArrayDin(array); i++)
+        {
+            pritf("%d. ", i + 1);
+            printWord(array.detil_playlist[i].nama_PlayList);
+            printf("\n");
+        }
+    }
+    printf("Masukkan ID Playlist: ");
+    STARTINPUT();
+    int hasil = strToInteger(currentInput);
+    Word PANGGIL_PLAYLIST;
+    PANGGIL_PLAYLIST = array.detil_playlist[hasil].nama_PlayList;
+    printf("Memutar playlist “");
+    printWord(PANGGIL_PLAYLIST);
+    printf("”.\n");
+}
